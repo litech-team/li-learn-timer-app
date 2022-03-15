1084188d4852d8f9e3ac9e51f3f25f61fd29d252
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2022-03-16 07:14:14 +0900

    vue-routerの設定を修正

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from "vue-router"
 import routes from "~pages"
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 })
 
 export default router
