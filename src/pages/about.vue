8c90678c6f4c8edcc66997638d45de54fbf872b7
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2022-03-16 06:27:33 +0900

    テスト用にリンクを設置

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>Welcome! This is about.vue Page!</h1>
+  <a href="/">Back to index.vue Page!</a>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>Welcome! This is index.vue Page!</h1>
+  <a href="/about">To about.vue Page!</a>
 </template>
