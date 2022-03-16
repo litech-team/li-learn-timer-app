d71dabcd22429463b6206a6c0bf8fa75a9fa9c23
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2022-03-16 12:22:42 +0900

    useRegisterSWを呼ぶように変更

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <v-app>
     <v-main>
       <router-view />
+      <ReloadPrompt />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
+import ReloadPrompt from "./components/ReloadPrompt.vue"
 
 export default defineComponent({
-  name: "App",
+    name: "App",
+    components: { ReloadPrompt }
 })
 </script>
--- src/env.d.ts
+++ src/env.d.ts
@@ -8,6 +8,25 @@ declare module '*.vue' {
   export default component
 }
 
+declare module 'virtual:pwa-register/vue' {
+  // @ts-ignore ignore when vue is not installed
+  import type { Ref } from 'vue'
+
+  export type RegisterSWOptions = {
+    immediate?: boolean
+    onNeedRefresh?: () => void
+    onOfflineReady?: () => void
+    onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void
+    onRegisterError?: (error: any) => void
+  }
+
+  export function useRegisterSW(options?: RegisterSWOptions): {
+    needRefresh: Ref<boolean>
+    offlineReady: Ref<boolean>
+    updateServiceWorker: (reloadPage?: boolean) => Promise<void>
+  }
+}
+
 declare module 'vuetify'
 declare module 'vuetify/lib/components'
 declare module 'vuetify/lib/directives'
